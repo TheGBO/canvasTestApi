4b28a1b59a7235527acf3ea53f8352850d7b164b
TheGBO <gabrielthejota@gmail.com>
2021-12-04 12:46:19 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+app = express();
+
+
+var objectPosition = {
+    x: 0,
+    y: 0
+}
+
+app.use(express.json())
+
+app.get('/position', (req, res) => {
+    res.json(objectPosition);
+});
+
+app.put('/position', (req, res) => {
+    data = req.body;
+    objectPosition.x += data.x;
+    objectPosition.y += data.y;
+    res.json(objectPosition);
+});
+
+app.patch('/position', (req, res) => {
+    data = req.body;
+    objectPosition.x = data.x;
+    objectPosition.y = data.y;
+    res.json(objectPosition);
+});
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Listening at ${process.env.PORT}`);
+});
